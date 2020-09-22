85f522a5ec13a31759137552a48002dfc38beebc
Angelo <madhav1004@icloud.com>
2020-09-22 08:49:00 +0530

    now password is secure

--- teams.py
+++ teams.py
@@ -8,7 +8,6 @@ password = getpass()
 driver = webdriver.Chrome('./chromedriver.exe')
 
 
-
 driver.get('https://login.microsoftonline.com/common/oauth2/authorize?response_type=id_token&client_id=5e3ce6c0-2b1f-4285-8d4b-75ee78787346&redirect_uri=https%3A%2F%2Fteams.microsoft.com%2Fgo&state=3b4b794a-ec86-4706-a089-6168db7e6cbc&&client-request-id=970bbab7-b54f-423f-b08a-95f47b51ede8&x-client-SKU=Js&x-client-Ver=1.0.9&nonce=17e9bbad-3065-4cac-94cc-656eb9325b73&domain_hint=')
 
 driver.implicitly_wait(20)
